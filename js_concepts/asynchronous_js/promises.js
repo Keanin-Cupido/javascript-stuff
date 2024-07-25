2637d7343f6f577885852e9c04c7d18659c5609d
Keanin-Cupido <keanin16@gmail.com>
2024-07-25 22:56:15 +0200

    learn promises

---
+++ js_concepts/asynchronous_js/promises.js
@@ -0,0 +1,25 @@
+// Promises
+
+const promise = new Promise((resolve, reject) => {
+	const allWentWell = false;
+	if (allWentWell == true) {
+		resolve("All things went well.")
+	} else {
+		reject("Oops, something went wrong.");
+	}
+});
+
+const pendingPromise = new Promise((resolve, reject) => {
+	const randomNumber = Math.floor(Math.random() * 10);
+
+	console.log(randomNumber)
+
+	setTimeout(() => {
+		if (randomNumber < 4) {
+			resolve("Well done, you guessed right!");
+		} else {
+			reject("Oops! You guessed wrong!")
+		}
+	}, 2000);
+});
+
