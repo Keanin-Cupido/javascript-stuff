96e570731e8d8b0a8c630f6315cf2397202bd4c8
Keanin-Cupido <keanin16@gmail.com>
2024-07-29 16:16:34 +0200

    promises then and catch

--- js_concepts/asynchronous_js/promises.js
+++ js_concepts/asynchronous_js/promises.js
@@ -1,7 +1,8 @@
 // Promises
 
 const promise = new Promise((resolve, reject) => {
-	const allWentWell = false;
+	const allWentWell = false
+	;
 	if (allWentWell == true) {
 		resolve("All things went well.")
 	} else {
@@ -9,17 +10,22 @@ const promise = new Promise((resolve, reject) => {
 	}
 });
 
-const pendingPromise = new Promise((resolve, reject) => {
-	const randomNumber = Math.floor(Math.random() * 10);
+// const pendingPromise = new Promise((resolve, reject) => {
+// 	const randomNumber = Math.floor(Math.random() * 10);
 
-	console.log(randomNumber)
+// 	console.log(randomNumber)
 
-	setTimeout(() => {
-		if (randomNumber < 4) {
-			resolve("Well done, you guessed right!");
-		} else {
-			reject("Oops! You guessed wrong!")
-		}
-	}, 2000);
-});
+// 	setTimeout(() => {
+// 		if (randomNumber < 4) {
+// 			resolve("Well done, you guessed right!");
+// 		} else {
+// 			reject("Oops! You guessed wrong!")
+// 		}
+// 	}, 2000);
+// });
 
+promise.then((value) => {
+	console.log(value);
+}).catch((error) => {
+	console.log(error);
+})
